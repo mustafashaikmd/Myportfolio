f542ae90c562722ebc45a970f27d90e7fb76ba5e
Mustafa Shaik <shaikmustafa572@gmail.com>
2025-04-06 11:20:35 +0530

    commit

---
+++ src/app/data.js
@@ -0,0 +1,138 @@
+/*
+Websites:
+
+- https://github.com/pmndrs/gltfjsx (GLTF JSX for 3D Models)
+- https://lucide.dev/icons/ (Lucide Icons)
+- https://github.com/anuraghazra/github-readme-stats (Github Readme Stats)
+- https://skillicons.dev (Skill Icons to show skills)
+- https://github-readme-streak-stats.herokuapp.com (Github Readme Streak Stats)
+
+:root {
+  --background: 27 27 27;
+  --foreground: 225 225 225;
+  --muted: 115 115 115;
+  --accent: 254 254 91; #FEFE5B
+}
+
+*/
+
+export const projectsData = [
+  {
+    id: 1,
+    name: "Art Gallery",
+    description: "A website designed to showcase the arts",
+    date: "2024-09-30",
+    demoLink: "https://mustafashaikmd.github.io/artgallery/",
+  },
+  {
+    id: 2,
+    name: "Twitter Clone",
+    description: "A Replica of the Twitter Website",
+    date: "2024-10-25",
+    demoLink: "https://mustafashaikmd.github.io/twitter_clone/",
+  },
+  {
+    id: 3,
+    name: "Car Rental",
+    description: "Plan and Rent a Car for your Travel",
+    date: "2024-11-12",
+    demoLink: "https://codepen.io/MustafaShaik/pen/RwXobYE",
+  },
+  {
+    id: 4,
+    name: "Booking Platform",
+    description: "Your travel buddy for all your bookings",
+    date: "2025-11-30",
+    demoLink: "https://mustafashaikmd.github.io/booking_platform/",
+  },
+  {
+    id: 5,
+    name: "Budget Tracker",
+    description: "An application to track all your expenses",
+    date: "2025-01-15",
+    demoLink: "https://mustafashaikmd.github.io/budget-tracker/",
+  },
+  {
+    id: 6,
+    name: "Brewed Awakening",
+    description: "A Coffee Shop Website",
+    date: "2025-02-01",
+    demoLink: "https://mustafashaikmd.github.io/brewed-awakening/",
+  },
+  {
+    id: 7,
+    name: "Innomatics Clone",
+    description: "A Replica of Innomatics.in Website",
+    date: "2025-02-15",
+    demoLink: "https://mustafashaikmd.github.io/innomatics_clone/",
+  },
+  {
+    id: 8,
+    name: "E-kart",
+    description: "An E-Commerce Shopping Website",
+    date: "2025-02-20",
+    demoLink: "https://mustafashaikmd.github.io/ekart/",
+  },
+  {
+    id: 9,
+    name: "Memory Game",
+    description: "An Interesting Memory Game",
+    date: "2025-02-25",
+    demoLink: "https://mustafashaikmd.github.io/Memory_game/",
+  },
+  {
+    id: 10,
+    name: "ToDoList",
+    description: "An Application to manage all your daily tasks",
+    date: "2025-03-02",
+    demoLink: "https://todolist-wheat-eta.vercel.app/",
+  },
+  {
+    id: 11,
+    name: "Iphone Clone",
+    description: "A Replica of Iphone webiste",
+    date: "2024-11-30",
+    demoLink: "https://iphone-nine-lovat.vercel.app/",
+  },
+  {
+    id: 12,
+    name: "MovieApp",
+    description: "An Application to keep the track of Movies",
+    date: "2025-02-10",
+    demoLink: "https://movie-app-theta-ten-82.vercel.app/",
+  },
+  {
+    id: 13,
+    name: "Bookstore",
+    description: "An E-BookStore Application",
+    date: "2025-02-27",
+    demoLink: "https://bookstore-eta-ivory.vercel.app/",
+  }
+];
+
+export const BtnList = [
+  { label: "Home", link: "/", icon: "home", newTab: false },
+  { label: "About", link: "/about", icon: "about", newTab: false },
+  { label: "Projects", link: "/projects", icon: "projects", newTab: false },
+  { label: "Contact", link: "/contact", icon: "contact", newTab: false },
+  {
+    label: "Github",
+    link: "https://github.com/mustafashaikmd",
+    icon: "github",
+    newTab: true,
+  },
+  {
+    label: "LinkedIn",
+    link: "https://www.linkedin.com/in/mustafashaik97",
+    icon: "linkedin",
+    newTab: true,
+  },
+  {
+    label: "Resume",
+    link: "/resume.pdf",
+    icon: "resume",
+    newTab: true,
+  },
+];
+
+
